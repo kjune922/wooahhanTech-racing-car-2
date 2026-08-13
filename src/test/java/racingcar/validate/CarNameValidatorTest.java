d5acd510d2c00af639cc3a3377817933d88d50c0
kjune922 <kjune922@naver.com>
2026-08-13 16:01:50 +0900

    test : 자동차 이름 null, blank 불가능, 5글자 까진가능 그 이상 불가능

--- src/test/java/racingcar/validate/CarNameValidatorTest.java
+++ src/test/java/racingcar/validate/CarNameValidatorTest.java
@@ -1,11 +1,8 @@
 package racingcar.validate;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import racingcar.car.Cars;
 
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarNameValidatorTest {
 
@@ -17,6 +14,36 @@ class CarNameValidatorTest {
                 carNameValidator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름은_5글자까지만가능 () {
+        String carName = "leeeeee";
+
+        CarNameValidator carNameValidator = new CarNameValidator();
+        assertThatThrownBy(() ->
+                carNameValidator.validateCarName(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+
+    }
+
+    @Test
+    void 자동차_이름은_빈이름_공백_불가능 () {
+        String carName1 = "";
+        String carName2 = null;
+
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        assertThatThrownBy(() ->
+                carNameValidator.validateCarName(carName1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
+
+        assertThatThrownBy(() ->
+                carNameValidator.validateCarName(carName2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
     }
 
 }

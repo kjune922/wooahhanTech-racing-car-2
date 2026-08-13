4cdbcda4176a27853ded50a1ce1f92d0b36a5b58
kjune922 <kjune922@naver.com>
2026-08-13 16:07:29 +0900

    refactor: CarNameValidatorTest refactor

--- src/test/java/racingcar/validate/CarNameValidatorTest.java
+++ src/test/java/racingcar/validate/CarNameValidatorTest.java
@@ -18,8 +18,8 @@ class CarNameValidatorTest {
 
     @Test
     void 자동차_이름은_5글자까지만가능 () {
-        String carName1 = "leeeeee";
-        String carName2 = "kim";
+        String carName1 = "leeeee";
+        String carName2 = "kimmm";
 
         CarNameValidator carNameValidator = new CarNameValidator();
         assertThatThrownBy(() ->
@@ -31,27 +31,29 @@ class CarNameValidatorTest {
                 .doesNotThrowAnyException();
 
     }
+    @Test
+    void 자동차_이름은_빈_문자열일_수_없다() {
+        CarNameValidator validator = new CarNameValidator();
+
+        assertThatThrownBy(() -> validator.validateCarName(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
+    }
 
     @Test
-    void 자동차_이름은_빈이름_공백_불가능 () {
-        String carName1 = "";
-        String carName2 = null;
-        String carName3 = "    ";
+    void 자동차_이름은_공백으로만_구성될_수_없다() {
+        CarNameValidator validator = new CarNameValidator();
 
-        CarNameValidator carNameValidator = new CarNameValidator();
-
-        assertThatThrownBy(() ->
-                carNameValidator.validateCarName(carName1))
+        assertThatThrownBy(() -> validator.validateCarName("   "))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
+    }
 
-        assertThatThrownBy(() ->
-                carNameValidator.validateCarName(carName2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
+    @Test
+    void 자동차_이름은_null일_수_없다() {
+        CarNameValidator validator = new CarNameValidator();
 
-        assertThatThrownBy(() ->
-                carNameValidator.validateCarName(carName3))
+        assertThatThrownBy(() -> validator.validateCarName(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 비어 있을 수 없습니다.");
     }
